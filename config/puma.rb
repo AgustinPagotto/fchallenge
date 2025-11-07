a8887c328a81dde575868093bf37f9a34fb68c65
AgustinPagotto <agustinpagotto@gmail.com>
2025-11-06 22:27:32 -0300

    update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,15 @@
 # Threads
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
+threads_count = 5
 threads threads_count, threads_count
 
 # Processes per worker
-workers ENV.fetch("WEB_CONCURRENCY", 0)
+workers 0
 
 # Rackup file
 rackup "config.ru"
 
 # Port
-port ENV.fetch("PORT", 9292)
+port 9292
 
 # Environment
-environment ENV.fetch("RACK_ENV", "development")
+environment "development"
