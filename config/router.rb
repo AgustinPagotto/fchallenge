1fb4e355d7cca5a1fb5941e885b6e070a5e54e28
AgustinPagotto <agustinpagotto@gmail.com>
2025-11-09 18:37:20 -0300

    update routes to manage calling controllers

--- config/router.rb
+++ config/router.rb
@@ -4,14 +4,25 @@ class Router
   end
 
   def route!
-    if @request.path == "/"
-      [200, { "Content-Type" => "text/plain"}, ["Hello"]]
-    else
-      not_found
+    if klass = controller_class
+      add_route_info_to_request_params!
+
+      controller = klass.new(@request)
+      action = route_info[:action]
+
+      if controller.respond_to?(action)
+        puts "\nRouting to #{klass}##{action}"
+        return controller.public_send(action)
+      end
     end
+    not_found
   end
 
   private
+
+  def add_route_info_to_request_params
+    @request.params.merge!(route_info)
+  end
 
   def not_found(msg = "Not Found")
     [404, {"Content-Type" => "text/plain"}, [msg]]
@@ -46,5 +57,16 @@ class Router
 
   def path_fragments
     @fragments ||= @request.path.split("/").reject { |s| s.empty? }
+  end
+
+  def controller_name
+    "#{route_info[:resource].capitalize}Controller"
+  end
+
+  #Returns the class of the controllers name
+  def controller_class
+    Object.const_get(controller_name)
+  rescue NameError
+    nil
   end
 end
