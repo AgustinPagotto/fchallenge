d9b21740881cbeb7719a1fea69533469497781a5
AgustinPagotto <agustinpagotto@gmail.com>
2025-11-09 14:58:59 -0300

    update routes to create a hash of action

--- config/router.rb
+++ config/router.rb
@@ -16,5 +16,35 @@ class Router
   def not_found(msg = "Not Found")
     [404, {"Content-Type" => "text/plain"}, [msg]]
   end
-end
 
+  #route info returns a hash with the "routing coordinates"
+  #First element = resource
+  #Second element = action to do (can be show, new, index or create depending of the http method
+  #and the id
+  #Third id if it exists
+  def route_info
+    @route_info ||= begin
+                      resource = path_fragments[0] || "base"
+                      id, action = find_id_and_action(path_fragments[1])
+                      { resource: resource, action: action, id: id}
+                    end
+  end
+
+
+  def find_id_and_action(fragment)
+    case fragment
+    when "new"
+      [nil, :new]
+
+    when nil
+      action = @request.get? ? :index : :create
+      [nil, action]
+    else
+      [fragment, :show]
+    end
+  end
+
+  def path_fragments
+    @fragments ||= @request.path.split("/").reject { |s| s.empty? }
+  end
+end
