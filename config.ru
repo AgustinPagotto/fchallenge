a053c7860d6f25322f626a7a05df78ee4faf5628
AgustinPagotto <agustinpagotto@gmail.com>
2025-11-11 10:31:27 -0300

    add middleware to auth the user

--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 require_relative "app"
+require_relative "middleware/auth_middleware"
 require "dotenv/load"
 
 use Rack::Reloader, 0
+use AuthChecker
 run App.new
---
+++ middleware/auth_middleware.rb
@@ -0,0 +1,33 @@
+class AuthChecker
+  def initialize(app)
+    @app = app 
+  end
+
+  def call(env)
+    path = env["PATH_INFO"]
+    if path == "/auth" || path == "/openapi.yaml" || path == "/AUTHORS"
+      @app.call(env)
+    end
+    auth_header = env["HTTP_AUTHORIZATION"]
+    if valid_token?(auth_header)
+      @app.call(env)
+    else
+      [
+        401,
+        { "Content-Type" => "application/json"},
+        [{ error: "Unauthorized"}.to_json]
+      ]
+    end
+  end
+
+  private
+
+  def valid_token?(auth_header)
+    return false if auth_header.nil?
+
+    # Style of header: "Bearer secret-token"
+    token = auth_header.split(" ").last
+    token == ENV.fetch("AUTH_TOKEN", "secret-token")
+  end
+    
+end
